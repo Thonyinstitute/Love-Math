c92bf6dfc83ad2ee8e9701ea5146636bd24da04c
Thonyinstitute <personally33@gmail.com>
2023-02-22 20:31:52 +0000

    button and alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for ( let button of buttons) {
+        button.addEventListener ("click", function() {
+            if(this.getAttribute("data-type") ==="submit") {
+                alert("You click Submit!");
+            } else {
+                let gametype = this.getAttribute("data-type");
+                alert(`You Clicked ${gametype}`);
+            }
+        })
+
+    }
+})
+
+
+
+
+
 function rungame() {
 
 }
