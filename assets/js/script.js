8517a5041ca45cf362e590024d495e84272900c8
Thonyinstitute <personally33@gmail.com>
2023-02-27 10:46:58 +0000

    addition is added or math.random

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,15 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit");   
             } else{
                 let gametype=this.getAttribute("data-type");
+
                 alert(`You clicked ${gametype}`);
+                rungame(gametype);
             }
         })
     }
+
+
+    rungame("addition");
 
 })
 
@@ -23,9 +28,18 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
 */
 
-function rungame() {
+function rungame(gametype) {
+
+    // create two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if(gametype==="addition") {
+        displayAdditionQuestion(num1, num2);
+    }else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gametype}. Arborting!`;
+    }
 }
 
 function checkAnswer () {
@@ -45,8 +59,11 @@ function  incrementWrongAnswer() {
 
 }
 
-function  displayAdditionQuestion() {
-
+function  displayAdditionQuestion(operand1, operand2) {
+    
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function  displaySubtractQuestion() {
